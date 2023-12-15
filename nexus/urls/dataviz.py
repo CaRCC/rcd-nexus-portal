25b87275354bf1638def1da7f0a2038de912c096
Patrick Schmitz <patrick@sempercogito.com>
2023-12-15 16:25:22 +0000

    Sketched out all the main templates for high-level data viz navigation, etc. Added breadcrumbs support.

--- nexus/urls/dataviz.py
+++ nexus/urls/dataviz.py
@@ -6,4 +6,10 @@ app_name = "dataviz"
 
 urlpatterns = [
     path("", data_viz_main, name="vizmain"),
+    path("demographics", data_viz_demographics, name="demographics"),
+    path("demographics/mapviews", data_viz_demographics_maps, name="demographics_mapviews"),
+    path("demographics/chartviews", data_viz_demographics_charts, name="demographics_chartviews"),
+    path("demographics/scatterplots", data_viz_demographics_scatter, name="demographics_scatterplots"),
+    path("capsmodeldata", data_viz_capsmodeldata, name="capsmodeldata"),
+    path("prioritiesdata", data_viz_prioritiessdata, name="prioritiesdata"),
 ]
--- nexus/views/dataviz.py
+++ nexus/views/dataviz.py
@@ -3,7 +3,68 @@ from django.conf import settings
 from django.http import HttpRequest
 from django.shortcuts import redirect, render
 
+def data_viz_main(request):
+    context = {
+        "breadcrumbs":{
+            "Data Viewer":"dataviz:vizmain",
+            }
+        }
+    return render(request, "dataviz/vizmain.html", context)
 
-def data_viz_main(request): 
-    return render(request, "dataviz/vizmain.html", {})
+def data_viz_demographics(request): 
+    context = {
+        "breadcrumbs":{
+            "Data Viewer":"dataviz:vizmain",
+            "Community Demographics":"dataviz:demographics",
+            }
+        }
+    return render(request, "dataviz/demographics.html", context)
+
+def data_viz_demographics_maps(request): 
+    context = {
+        "breadcrumbs":{
+            "Data Viewer":"dataviz:vizmain",
+            "Community Demographics":"dataviz:demographics",
+            "Map Views":"dataviz:demographics_mapviews",
+            }
+        }
+    return render(request, "dataviz/mapviews.html", context)
+
+def data_viz_demographics_charts(request): 
+    context = {
+        "breadcrumbs":{
+            "Data Viewer":"dataviz:vizmain",
+            "Community Demographics":"dataviz:demographics",
+            "Chart Views":"dataviz:demographics_cartviews",
+            }
+        }
+    return render(request, "dataviz/chartviews.html", context)
+
+def data_viz_demographics_scatter(request): 
+    context = {
+        "breadcrumbs":{
+            "Data Viewer":"dataviz:vizmain",
+            "Community Demographics":"dataviz:demographics",
+            "Scatter Plots":"dataviz:demographics_scatterplots",
+            }
+        }
+    return render(request, "dataviz/scatterplots.html", context)
+
+def data_viz_capsmodeldata(request): 
+    context = {
+        "breadcrumbs":{
+            "Data Viewer":"dataviz:vizmain",
+            "Capabilities Model Data":"dataviz:capsmodeldata",
+            }
+        }
+    return render(request, "dataviz/capsmodeldata.html", context)
+
+def data_viz_prioritiessdata(request): 
+    context = {
+        "breadcrumbs":{
+            "Data Viewer":"dataviz:vizmain",
+            "Priorities Data":"dataviz:prioritiesdata",
+            }
+        }
+    return render(request, "dataviz/prioritiesdata.html", context)
 
