852b231e96b630c7b79f4973c160a36075fbd12c
pjfischer <p.fischer@utah.edu>
2024-03-29 23:10:23 +0000

    Fix remaining dv1.beta issues (contributor status, profile import)

--- nexus/models/users.py
+++ nexus/models/users.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -13,3 +14,10 @@ class User(AbstractUser):
         if not self.first_name:
             return ""
         return f"{self.first_name} {self.last_name}"
+    
+    @property
+    def is_capmodel_contributor(self):
+        for profile in self.rcd_profiles.filter(year__gte=timezone.now()-timezone.timedelta(days=365*3)):
+            if profile.capabilities_assessment.review_status == "approved":
+                return True
+        return False
