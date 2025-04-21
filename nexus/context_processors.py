a9061a0ff9283f6b735b2d2f3124e7eb5c579f83
Patrick Schmitz <patrick@sempercogito.com>
2025-04-21 23:56:24 +0000

    Fix for issue #264 to centralize url and strings related to the license.

--- nexus/context_processors.py
+++ nexus/context_processors.py
@@ -1,4 +1,5 @@
 from nexus.models import MOTD 
+from django.conf import settings
 
 def base_template(request):
     template = "base.html"
@@ -7,7 +8,12 @@ def base_template(request):
         #if request.modal:
         #    template = "base_modal.html"
 
-    return {"base_template": template}
+    return {
+        "base_template": template,
+        "cc_license_url" : settings.CC_LICENSE_URL,
+        "cc_license_string" : settings.CC_LICENSE_STRING,
+        "cc_license_desc" : settings.CC_LICENSE_DESC,
+        }
 
 def motd_message(request):
     message, dismissDays = MOTD.getMOTD()
